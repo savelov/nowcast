9c9a7054195b83071ac8ff5506be60c8f1fd9668
Eugene Savelov <savelov@gmail.com>
2019-07-30 23:13:42 +0300

    change back to 14 lead times

--- examples/probabilistic_nowcast_with_export.py
+++ examples/probabilistic_nowcast_with_export.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 import os, sys
-import pyximport
-pyximport.install()
+#import pyximport
+#pyximport.install()
 
 from tendo import singleton
 from glob import glob
@@ -69,7 +69,7 @@ decomp_method       = "fft"
 
 ## forecast parameters
 n_prvs_times        = 5                # use at least 9 with DARTS
-n_lead_times        = 4
+n_lead_times        = 14
 n_ens_members       = 5
 n_cascade_levels    = 6
 ar_order            = 2
