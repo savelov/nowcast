9bd0e194517a91f5f583ae86c0bee46f5ce8c7be
Nabeel07925 <muhammad.nabeel38@yahoo.com>
2019-07-30 21:31:14 +0500

    fix(): fix inverted geotiff issue

--- pysteps_custom_utils/probability_nowcasting.py
+++ pysteps_custom_utils/probability_nowcasting.py
@@ -5,5 +5,5 @@ import pysteps as stp
 def nowcast_probability(n_time_step, shape, R_fct):
     prob = np.zeros((n_time_step, shape[0], shape[1]))
     for i in range(n_time_step):
-        prob[i, :, :] = stp.postprocessing.ensemblestats.excprob(R_fct[:, -1, :, :], 0.1, ignore_nan=True)
+        prob[i, :, :] = stp.postprocessing.ensemblestats.excprob(R_fct[:, i, :, :], 0.1, ignore_nan=True)
     return prob
